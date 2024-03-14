bfb16ecf83991f2e40588a73b44fe83ec707f38b
MountainGod2 <admin@reid.ca>
2024-03-14 11:25:22 -0600

    refactor: Add src path and create ci-cd.yml

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from cb_events import CBAPIPoller, log_events
 
 
 async def main() -> None:  # noqa: D103
-    url = "https://eventsapi.chaturbate.com/events/mrslaw/JAzuRHJWcoVcHjIDLs7bjNlQ/"
+    url = "https://eventsapi.chaturbate.com/events/USER_NAME/API_KEY/"
     async with CBAPIPoller(url) as poller:
         await poller.fetch_events(event_callback=log_events)
 
