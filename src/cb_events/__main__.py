76586581c4b56d232fd675ad750dac8ce69b4d57
MountainGod2 <admin@reid.ca>
2024-03-15 14:17:53 -0600

    feat: Raise exception if BASE_URL variable is not set when using program as a module

--- src/cb_events/__main__.py
+++ src/cb_events/__main__.py
@@ -6,13 +6,18 @@ import os
 from dotenv import load_dotenv
 
 from cb_events import CBAPIPoller
+from cb_events.exceptions import BaseURLError
 
-load_dotenv(override=False)
+load_dotenv(override=True)
 
 
 async def main() -> None:
     """Run the event poller."""
     url = os.environ.get("BASE_URL")
+    if not url:
+        error_msg = "Please set the BASE_URL environment variable with the correct URL."
+        raise BaseURLError(error_msg)
+
 
     async with CBAPIPoller(url) as poller:
         await poller.fetch_events()
